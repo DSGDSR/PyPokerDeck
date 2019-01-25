937f8a21b9df396f77a10ae9aa3d6cfba8d66e98
DSGDSR <davidsanrubio@gmail.com>
2019-01-25 08:32:33 +0100

    Added README.md and .gitignore

--- deck.py
+++ deck.py
@@ -2,13 +2,13 @@ import json, random
 from card import Card
 
 class Deck():
-    def __init__(self):
+    def __init__(self, shuffle=False):
         with open('deck.json') as data:
             cards = json.load(data)["cards"]
             self.number_of_cards = len(cards)
             self.suits = list(set([s.get("suit") for s in cards]))
             self.codes = [s.get("code") for s in cards]
-            self.shuffled = False
+            self.shuffled = shuffle
             deck = []
             for c in cards:
                 suit = c.get("suit")
@@ -17,6 +17,8 @@ class Deck():
                 images = c.get("images")
                 deck.append(Card(value, suit, code, images))
             self.deck = deck
+            if self.shuffled:
+                self.shuffle()
 
     def drawCards(self, t):
         cards = self.deck[self.number_of_cards-t:]
@@ -24,8 +26,8 @@ class Deck():
         self.number_of_cards = len(self.deck)
         return cards
 
-    def shuffle(self, t):
-        [random.shuffle(self.deck) for _ in range(0,t)]
+    def shuffle(self, times=1):
+        [random.shuffle(self.deck) for _ in range(0,times)]
         self.shuffled = True
         return self.deck
 
@@ -35,7 +37,8 @@ class Deck():
     def searchCard(self, code):
         for c in self.deck:
             if code == c.get("code"):
-                return True
+                return c
+        return None
 
     def removeCard(self, code):
         if code in self.codes:
@@ -43,24 +46,28 @@ class Deck():
                 if code == c.code:
                     self.deck.remove(c)
                     self.number_of_cards = len(self.deck)
-                    return True
+                    return c
         else:
             return False
 
 
 
 if __name__ == "__main__":
-    deck = Deck()
-    deck.shuffle(5)
+    deck = Deck(shuffle=True)
+
+    # Custom shuffle
+    deck.shuffle(times=2)
+
+    # Draw 2 cards returning a list wiht them
     nc = deck.drawCards(2)
-    nc = deck.drawCards(3)
-    print([c.code for c in nc], deck.number_of_cards, "\n")
 
+    # Reset the deck without shuffling
     deck.reset()
-    print([c.code for c in deck.deck], deck.number_of_cards, "\n")
 
-    if deck.removeCard('8S'):
-        print("8S removed")
-    else:
-        print("Error")
-    print([c.code for c in deck.deck], deck.number_of_cards, "\n")
+    # Removing a card returns its object
+    cc = deck.removeCard('8S')
+    print("8S removed", cc.code)
+
+    # Searching a card returns its object
+    # (None in case not found)
+    sc = deck.searchCard('8S')
